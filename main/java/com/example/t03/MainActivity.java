faf828bde4182ab6590dbf91130d54fba2c47d31
will <2017730059@student.unpar.ac.id>
2021-10-17 21:12:49 +0700

    nyetel drawer pake menu item

--- main/java/com/example/t03/MainActivity.java
+++ main/java/com/example/t03/MainActivity.java
@@ -1,22 +1,26 @@
 package com.example.t03;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.t03.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     ActivityMainBinding bindingMain;
 
     DrawerLayout drawer;
@@ -55,9 +59,15 @@ public class MainActivity extends AppCompatActivity {
 
         FragmentTransaction ft = this.fm.beginTransaction();
 
-        ft.add(this.bindingMain.fragmentContainer.getId(), this.fragmentMain)
-                .addToBackStack(null)
-                .commit();
+
+                                                                                                    // supaya awal dibuka app, ini ditampilkan awal
+        if (savedInstanceState == null) {                                                           // to avoid re-'creating' this fragment when rotating, so we with this, by rotating our savedinstance wont be null
+            ft.add(this.bindingMain.fragmentContainer.getId(), this.fragmentMain)                   // but this means when we rotate (it destroyed and recreated, n this will be called again
+                    .addToBackStack(null)
+                    .commit();
+        }
+
+
         //
                                                                                                     Log.d(TAG, "onCreate:   pasang toolbar");
         // pasang toolbar sbg actionbar
@@ -66,16 +76,63 @@ public class MainActivity extends AppCompatActivity {
 
         // nambah tombol 'hamburger' (ga mirip) (tombol menu biar buka nav bar nya)
         //                                                                  gaperlu this?               untuk apa?
-        ActionBarDrawerToggle abdt = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.openDrawer, R.string.closeDrawer);
+        ActionBarDrawerToggle abdt = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.openDrawer, R.string.closeDrawer);
         drawer.addDrawerListener(abdt);
-        abdt.syncState();
+        abdt.syncState();                                                                           // "take care of rotating the hamburger together drawer itself"
 
 
-        //
+
+        // untk menu berfungsi,
+        // krn pake Navigation Drawer, sama kaya button di activity, kita listen kl ditekan
+        NavigationView navigationView = bindingMain.navView;
+        navigationView.setNavigationItemSelectedListener(this);                                     // bisa set anonymous funciton /callback disini untk tiap ditekan new NavigationView.OnNavigationItemSelectedListener() { ....
+                                                                                                    // tp biar rapi, kita pake this (we pass this mainact), jd mesti implement lagi
 
                                                                                                     Log.d(TAG, "onCreate:   end of oncreate");
     }
 
+    //
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {                                                                 // ? kl pake binding??
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(this.bindingMain.fragmentContainer.getId(),
+                        new MainFragment()).commit();
+                break;
+            case R.id.nav_page2:
+                getSupportFragmentManager().beginTransaction().replace(this.bindingMain.fragmentContainer.getId(),
+                        new SecondFragment()).commit();
+                break;
+            case R.id.nav_exit:
+                // 'are u sure'?
+                // keluar aplikasi
+                closeApplication();
+                break;
+        }
+
+        // then we close our drawer
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+                                                                                                    // so when we go back, we dont leave the activity immediately, we just close our navigation drawer
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {                                             // kl mau tutup drawer yg sblh kanan, GravityCompat.END
+            drawer.closeDrawer(GravityCompat.START);
+        } else {                                                                                    // kl sdh tertutup, baru close activity nya
+            super.onBackPressed();
+        }
+    }
+
+    public void closeApplication () {
+        this.moveTaskToBack(true);
+        this.finish();
+    }
+
+
+    // kl biar pake ini gimana?
     public void changePage (int page) {
         FragmentTransaction ft = this.fm.beginTransaction();
         if (page == 1) {
@@ -92,4 +149,5 @@ public class MainActivity extends AppCompatActivity {
         }
         ft.commit();
     }
+
 }
