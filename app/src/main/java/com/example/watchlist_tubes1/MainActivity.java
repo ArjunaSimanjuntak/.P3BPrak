550bcf7e33f612d44e93e14f29a215ff6d800c1a
will <2017730059@student.unpar.ac.id>
2021-10-19 14:51:06 +0700

    tambah layout header untk di bagian atas navigation drawer, trs nambah toolbar supaya gantikan default biasa, terus tambah nav drawer nya disanan (hamburger/tombol bukak menu kiri).. tp belum berfungsi yg di drawer nya, blm pindah ke fragment/act lain

--- app/src/main/java/com/example/watchlist_tubes1/MainActivity.java
+++ app/src/main/java/com/example/watchlist_tubes1/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.watchlist_tubes1;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.watchlist_tubes1.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.binding = ActivityMainBinding.inflate(this.getLayoutInflater());                       // binding
+        View layout = binding.getRoot();
+        setContentView(layout);
+
+
+        Toolbar toolbar = binding.toolbar;                                                          // modul Praktikum_03 v2    & https://youtu.be/zYVEMCiDcmY
+        setSupportActionBar(toolbar);
+
+        drawer = binding.drawerLayout;
+
+        ActionBarDrawerToggle abdt = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.openDrawer, R.string.closeDrawer);
+        drawer.addDrawerListener(abdt);
+        abdt.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {                                                                   // kl nekan back button
+        if (this.drawer.isDrawerOpen(GravityCompat.START)) {                                        // n drawer lg kebukak
+            this.drawer.closeDrawer(GravityCompat.START);                                           // tutup
+        } else {                                                                                    // kl engga, back kaya biasa
+            super.onBackPressed();
+        }
     }
 }
