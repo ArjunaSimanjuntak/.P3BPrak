ae410cffb565ad30ec54658b9e42a5add6cf9cbd
JeremyW2017730059 <2017730059@student.unpar.ac.id>
2021-10-24 16:15:54 +0700

    isi oncreate di kelas DB nya

--- app/src/main/java/com/example/watchlist_tubes1/DatabaseHelper.java
+++ app/src/main/java/com/example/watchlist_tubes1/DatabaseHelper.java
@@ -8,14 +8,43 @@ import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
+    private static final String DATABASE_NAME = "movie.db";                                         // nama db yg diset di constructor
+
+    private static final String TABLE_NAME = "MOVIE_TABLE";                                         // nama" kolomnya
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_TITLE = "title_movie";
+    private static final String COLUMN_SYNOPSIS = "synopsis_movie";
+    private static final String COLUMN_REVIEW = "review_movie";
+    private static final String COLUMN_STATUS = "status_movie";
+    private static final String COLUMN_STAR = "howmanyStar_movie";
+
     public DatabaseHelper(@Nullable Context context ) {                                             //, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, "movie.db", null, 1);
+        super(context, DATABASE_NAME, null, 1);
     }
 
     // yg pertama kali dipanggil waktu DB (database) diakses
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        //
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + TABLE_NAME +
+                " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_TITLE + " TEXT, " +
+                COLUMN_SYNOPSIS + " TEXT, " +
+                COLUMN_REVIEW + " TEXT, " +
+                COLUMN_STATUS + " BOOLEAN, " +
+                COLUMN_STAR + " INTEGER);";
+
+        /*
+        CREATE TABLE
+            TABLE_NAME
+        ( COLUMN_ID         INTEGER PRIMARY KEY AUTOINCREMENT,
+          COLUMN_TITLE      TEXT,
+          COLUMN_SYNOPSIS   TEXT,
+          COLUMN_REVIEW     TEXT,
+          COLUMN_STATUS     BOOLEAN,
+          COLUMN_STAR       INTEGER );
+        * */
+
+        db.execSQL(query);
     }
 
     // dipanggil waktu versi db beruba
