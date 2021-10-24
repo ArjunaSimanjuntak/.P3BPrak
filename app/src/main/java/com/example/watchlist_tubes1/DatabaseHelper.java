2107fb41669da22943d1d1e87bc7bec9aa401147
JeremyW2017730059 <2017730059@student.unpar.ac.id>
2021-10-24 16:00:32 +0700

    perbaikin error di kelas DB, buat constructor ny

--- app/src/main/java/com/example/watchlist_tubes1/DatabaseHelper.java
+++ app/src/main/java/com/example/watchlist_tubes1/DatabaseHelper.java
@@ -1,9 +1,16 @@
 package com.example.watchlist_tubes1;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import androidx.annotation.Nullable;
+
 public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public DatabaseHelper(@Nullable Context context ) {                                             //, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, "movie.db", null, 1);
+    }
 
     // yg pertama kali dipanggil waktu DB (database) diakses
     @Override
@@ -11,8 +18,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         //
     }
 
+    // dipanggil waktu versi db beruba
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {                           // i oldVersion, i1 newVersion
         //
     }
 }
