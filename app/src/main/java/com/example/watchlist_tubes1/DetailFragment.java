13bcfae7d335873b2635d4e6d06d5a6eb3c31048
ArjunaSimanjuntak <arjunasimanjuntak48@gmail.com>
2021-10-28 16:40:09 +0700

    Detail Fragment Edited

--- app/src/main/java/com/example/watchlist_tubes1/DetailFragment.java
+++ app/src/main/java/com/example/watchlist_tubes1/DetailFragment.java
@@ -1,6 +1,43 @@
 package com.example.watchlist_tubes1;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class DetailFragment extends Fragment {
+import com.example.watchlist_tubes1.databinding.FragmentAddFilmBinding;
+import com.example.watchlist_tubes1.databinding.FragmentDetailBinding;
+
+public class DetailFragment extends Fragment implements View.OnClickListener {
+    FragmentDetailBinding binding;
+    public static DetailFragment newInstance() {
+        DetailFragment detailFragment = new DetailFragment();
+        Bundle args = new Bundle();
+        detailFragment.setArguments(args);
+        return detailFragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentDetailBinding.inflate(inflater, container, false);
+        View view =binding.getRoot();
+        binding.btnSave.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        //Belum selesai
+        Bundle result = new Bundle();
+        if(view==binding.btnSave){
+            result.putInt("page", 3);
+            this.getParentFragmentManager()
+                    .setFragmentResult("changePage", result);
+        }
+    }
 }
