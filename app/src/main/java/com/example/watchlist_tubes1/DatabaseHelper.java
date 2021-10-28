a778ce310d096990f670fba3e16937d441758aff
JeremyW2017730059 <2017730059@student.unpar.ac.id>
2021-10-28 21:21:43 +0700

    nambah" di kelas db dan wishlistfrag

--- app/src/main/java/com/example/watchlist_tubes1/DatabaseHelper.java
+++ app/src/main/java/com/example/watchlist_tubes1/DatabaseHelper.java
@@ -14,8 +14,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "movie.db";                                         // nama db yg diset di constructor
 
-    private static final String TABLE_NAME = "MOVIE_TABLE";                                         // nama" kolomnya
-    private static final String COLUMN_ID = "id";
+    private static final String TABLE_NAME = "MOVIE_TABLE";
+
+    private static final String COLUMN_ID = "id";                                                   // nama" kolomnya
     private static final String COLUMN_TITLE = "title_movie";
     private static final String COLUMN_SYNOPSIS = "synopsis_movie";
     private static final String COLUMN_REVIEW = "review_movie";
@@ -55,19 +56,22 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     // dipanggil waktu versi db beruba
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {                           // i oldVersion, i1 newVersion
-        //
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {                           // i oldVersion, i1 newVersion
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
 
+    public static String getColumnId() {
+        return COLUMN_ID;
+    }
 
-
-    public Cursor getData(){
-        SQLiteDatabase db = this.getWritableDatabase();
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getReadableDatabase();
         String query = "SELECT * FROM " + TABLE_NAME;
 
         Cursor cursor = null;
         if(db != null){
-            cursor = db.rawQuery(query, null);
+            cursor = db.rawQuery(query, null);                                          // diisi data sama datanya
         }
         return cursor;                                                                              // ada kemungkinan ngembaliin null
     }
@@ -86,21 +90,56 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         // query....
         SQLiteDatabase db = this.getWritableDatabase();                                             // writable karna mau nambah
 
-        ContentValues cv = new ContentValues();                                                     // prlu import. smcm hash map, atau array(?)
+        ContentValues values = new ContentValues();                                                     // prlu import. smcm hash map, atau array(?)
 
-        cv.put(COLUMN_TITLE, theMovie.getTitle());                                                  // key, value...
-        cv.put(COLUMN_SYNOPSIS, theMovie.getSynopsis());
-        cv.put(COLUMN_REVIEW, theMovie.getReview());
-        cv.put(COLUMN_STATUS, theMovie.getStatus());
-        cv.put(COLUMN_STAR, theMovie.getStar());
+        values.put(COLUMN_TITLE, theMovie.getTitle());                                                  // key, value...
+        values.put(COLUMN_SYNOPSIS, theMovie.getSynopsis());
+        values.put(COLUMN_REVIEW, theMovie.getReview());
+        values.put(COLUMN_STATUS, theMovie.getStatus());
+        values.put(COLUMN_STAR, theMovie.getStar());
 
                                                                                                     Log.d(TAG, "addMovie: the movie: " + theMovie.toString());
                                                                                                     // insert.
-        long result = db.insert(TABLE_NAME,null, cv);                                  // result + kl berhasil, - gagal
+                                                                                                    //  long result = db.insert(TABLE_NAME,null, values);                                  // result + kl berhasil, - gagal
+        // Insert the new row, returning the primary key value of the new row
+        long newRowId = db.insert(TABLE_NAME, null, values);
 
 
-        if( result < 0) { apaBisa = false; }                                                        // atau lgsg return false
+        if( newRowId < 0) { apaBisa = false; }                                                        // atau lgsg return false
 
         return apaBisa;
+    }
+
+
+    // waktu mau review
+                                                                            // status movie shrsnya gabisa diganti ?
+    void updateData(String row_id, String title, String synopsis, String review, String status, int star){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(COLUMN_TITLE, title);                                                            // key, value...
+        values.put(COLUMN_SYNOPSIS, synopsis);
+        values.put(COLUMN_REVIEW, review);
+        values.put(COLUMN_STATUS, status);
+        values.put(COLUMN_STAR, star);
+
+        long result = db.update(TABLE_NAME, values, "_id=?", new String[]{row_id});
+        if(result == -1){
+//            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+        }else {
+//            Toast.makeText(context, "Updated Successfully!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+    void deleteARow(String row_id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        long result = db.delete(TABLE_NAME, "_id=?", new String[]{row_id});
+        if(result == -1){
+//            Toast.makeText(context, "Failed to Delete.", Toast.LENGTH_SHORT).show();
+        }else{
+//            Toast.makeText(context, "Successfully Deleted.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/watchlist_tubes1/WishlistFragment.java
+++ app/src/main/java/com/example/watchlist_tubes1/WishlistFragment.java
@@ -1,6 +1,8 @@
 package com.example.watchlist_tubes1;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,13 +14,16 @@ import androidx.fragment.app.Fragment;
 
 import com.example.watchlist_tubes1.databinding.FragmentDaftarWatchlistBinding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WishlistFragment extends Fragment implements View.OnClickListener, MoviePresenter.IMoviePresenter {
+    private static final String TAG = "debug WishlistFrag";
     FragmentDaftarWatchlistBinding binding;
     private ListView listView;
     private WishlistAdapter adapter;
     private MoviePresenter moviePresenter;
+    private DatabaseHelper dbHelper;
 
     // dummy data
     public static Movie[] movieObjectArr = {
@@ -68,6 +73,41 @@ public class WishlistFragment extends Fragment implements View.OnClickListener, 
         this.listView.setAdapter(this.adapter);
 
         this.moviePresenter.loadData(movieObjectArr);
+
+                                                                                                    Log.d(TAG, "onCreateView: coba ambil data dr sqlite");
+        // coba load dr sqlite
+        if (dbHelper == null) {
+            dbHelper = new DatabaseHelper(this.getContext());
+        }
+
+        Cursor cursor = dbHelper.getAllData();                                                         // datanya yg diambil
+
+        List itemIds = new ArrayList<>();
+        long itemId = 1;
+
+        ArrayList<String> listMovieTitlesdrDB = new ArrayList<>();
+        String curMovieTitle = "something!";
+
+        while(cursor.moveToNext()) {
+            itemId = cursor.getLong(
+                    cursor.getColumnIndexOrThrow(dbHelper.getColumnId()));                          //
+            itemIds.add(itemId);
+//            listMoviedrDB.add(cursor.get)
+
+
+
+            // tes liat isinya
+            curMovieTitle = cursor.getString(1);                                                // ambil string di kolom ke2 (COLUMN_TITLE) krn dari 0, 1, 2...
+                                                                                                    Log.d(TAG, "onCreateView: isi titleMovie : " + curMovieTitle);
+            listMovieTitlesdrDB.add(curMovieTitle);
+        }
+
+
+
+
+        cursor.close();
+
+
 
         binding.btnAddFab.setOnClickListener(this);
 
